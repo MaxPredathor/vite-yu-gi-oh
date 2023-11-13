02175458bab68bd114fac87f5f956ddce466598c
MaxPredathor <max.gilli98@gmail.com>
2023-11-13 14:36:40 +0100

    fixes

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,14 @@
 </template>
 
 <script>
+import { store } './data/store.js'
   export default {
+    name: 'App',
     
   }
 </script>
 
 <style lang="scss" scoped>
+@use './assets/styles/partials/variables' as *;
 
 </style>
